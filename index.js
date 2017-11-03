8744e67c36eaec0dd82387b45d3a5c680c5a8aa3
Nick Peihl <nickp@sanjuanco.com>
2017-11-03 16:58:40 -0700

    Add documentation

--- index.js
+++ index.js
@@ -1,7 +1,47 @@
 var randomBytes = require('randombytes')
+var geojsonhint = require('@mapbox/geojsonhint')
 
 module.exports = GeoJson
 
+/**
+ * Create a GeoJson API to install into a p2p-db
+ * @see {@link https://github.com/noffle/p2p-db|p2p-db}
+ * @see {@link https://github.com/noffle/p2p-db-osm|p2p-db-osm}
+ * @see {@link https://github.com/mafintosh/hyperdb|hyperdb}
+ * @param {Function} db a hyperdb instance
+ * @param {Object} [opts]
+ * @param {string} [opts.prefix='/geojson'] A prefix to add to the hyperdb key
+ * @example
+ * var P2P = require('p2p-db')
+ * var GeoJson = require('p2p-db-geojson')
+ * var hyperdb = require('hyperdb')
+ * var ram = require('random-access-memory')
+ *
+ * var hyper = hyperdb(ram, { valueEncoding: 'json' })
+ * var db = P2P(hyper)
+ * db.install('geojson', new GeoJson(hyper))
+ *
+ * var gj = {
+ *   type: "Feature",
+ *   properties: {},
+ *   geometry: {
+ *     type: "Point",
+ *     coordinates: [
+ *       -123.017349,
+ *       48.535021
+ *     ]
+ *   }
+ * }
+ *
+ * db.geojson.create(gj, function (err, key) {
+ *   if (err) throw err
+ *   console.log(key)
+ *   db.geojson.get(key, function (err, feature) {
+ *     if (err) throw err
+ *     console.log(feature)
+ *   })
+ * })
+ */
 function GeoJson (db, opts) {
   if (!(this instanceof GeoJson)) return new GeoJson(db, opts)
   if (!db) throw new Error('missing param "db"')
@@ -12,8 +52,8 @@ function GeoJson (db, opts) {
 }
 
 GeoJson.prototype.create = function (element, cb) {
-  // TODO element check
-
+  var errors = geojsonhint.hint(element)
+  if (errors.length) cb(errors.join(', '))
   var id = generateId()
   var key = this._constructKey(id)
   element.id = id
@@ -24,8 +64,8 @@ GeoJson.prototype.create = function (element, cb) {
 }
 
 GeoJson.prototype.put = function (id, element, cb) {
-  // TODO element check
-
+  var errors = geojsonhint.hint(element)
+  if (errors.length) cb(errors.join(', '))
   var key = this._constructKey(id)
   this.db.put(key, element, function (err) {
     if (err) return cb(err)
